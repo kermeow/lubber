47430bbb1195f16edfef0149a8ef1e787aa68450
jayden <46307433+kermeow@users.noreply.github.com>
2025-03-09 02:54:03 +0000

    feat: reimplement init

--- lubber/app.py
+++ lubber/app.py
@@ -1,13 +1,17 @@
+import subprocess
 from pathlib import Path
-from rich import print
+import importlib.resources as resources
 
 import typer
+from rich import print
+from rich.prompt import Prompt, Confirm
+from semver import Version
 from typing_extensions import Annotated
 
 from lubber.models.config import GlobalConfig
 from lubber.models.project import Project
 from lubber.models.state import State
-from lubber.utils import validate_mod_id, suggest_mod_id
+from lubber.utils import get_username, suggest_mod_id, validate_mod_id, is_exe
 
 app = typer.Typer(
     no_args_is_help=True,
@@ -15,22 +19,102 @@ app = typer.Typer(
 )
 state = State()
 
+
 @app.command()
 def init(
     ctx: typer.Context,
     dir: Annotated[
         Path, typer.Argument(help="Specify the path to make the project in.")
     ] = None,
-    auto: Annotated[bool, typer.Option(help="Skips the interactive prompts.")] = False,
+    interactive: bool = True,
     name: str = None,
     version: str = None,
     desc: str = None,
     author: str = None,
+    git: bool = False,
 ):
     """
     Creates a new mod project in the specified directory.
     """
-    pass
+
+    if dir is not None:
+        state.project_path = dir.absolute()
+
+    state.project_path.mkdir(parents=True, exist_ok=True)
+
+    project_file = state.project_path / "lubber.toml"
+    if project_file.is_file():
+        raise Exception("Project already exists in directory.")
+
+    project: Project = Project()
+
+    if name is None:
+        name = suggest_mod_id(state.project_path.name)
+    if version is None:
+        version = "0.1.0"
+    if desc is None:
+        desc = ""
+    if author is None:
+        author = get_username()
+
+    if interactive:
+        name = Prompt.ask(
+            "Mod name",
+            default=name,
+        )
+
+        version = Prompt.ask("Mod version", default=version)
+
+        desc = Prompt.ask("Mod description", default=desc, show_default=False)
+
+        author = Prompt.ask("Mod author", default=author)
+
+        git = Confirm.ask("Initialise a Git repository?", default=git)
+
+    project.mod.name = name
+    project.mod.version = version
+    project.mod.description = desc
+    project.mod.authors = [author]
+
+    project.dependencies["sm64coopdx"] = "^1.0.0"
+
+    print(f"[blue]Created mod project '{project.mod.name}'.")
+
+    project.save(project_file)
+
+    assets_dir = state.project_path / project.directories.assets
+    assets_dir.mkdir(parents=True, exist_ok=True)
+    (assets_dir / ".gitkeep").touch()
+
+    src_dir = state.project_path / project.directories.source
+    src_dir.mkdir(parents=True, exist_ok=True)
+    (src_dir / "main.lua").write_text(
+        resources.read_text("lubber", "data/main.lua").format_map(
+            {"name": name, "desc": desc, "author": author}
+        )
+    )
+
+    (state.project_path / ".gitignore").write_text(
+        resources.read_text("lubber", "data/gitignore.txt")
+    )
+
+    existing_git = state.project_path / ".git"
+    if git and existing_git.is_dir():
+        print("[yellow]A Git repository already exists.")
+        git = False
+
+    if git and is_exe("git"):
+        print("[blue]Initialising Git repository...")
+        subprocess.call(["git", "init", "."], cwd=state.project_path)
+        subprocess.call(
+            ["git", "add", "lubber.toml", "src/*", ".gitignore"], cwd=state.project_path
+        )
+        subprocess.call(
+            ["git", "commit", "-m", "chore: init lubber project"],
+            cwd=state.project_path,
+        )
+    elif git:
+        print("[red]Git is not installed. A repository will not be created.")
 
     restore(ctx)
 
@@ -41,11 +125,11 @@ def restore(ctx: typer.Context) -> bool:
     Restores the specified project, making sure all dependencies are met.
     """
     if not state.project_path.is_dir():
-        raise Exception("Project directory doesn't exist")
+        raise Exception("Project directory doesn't exist.")
 
     project_file = state.project_path / "lubber.toml"
     if not project_file.is_file():
-        raise Exception("No project file in directory")
+        raise Exception("No project file in directory.")
 
     project: Project = Project.load_config(project_file)
 
@@ -103,5 +187,3 @@ def main(project: Path = typer.Option(None, help="Specify the path of the projec
 
     if project is not None:
         state.project_path = project.absolute()
-    if state.project_path.exists() and not state.project_path.is_dir():
-        raise typer.BadParameter("Invalid project directory")
--- lubber/utils.py
+++ lubber/utils.py
@@ -1,5 +1,7 @@
 import shutil
 import re
+import os
+import pwd
 
 strict_mod_id_regex: re.Pattern = re.compile(r"[A-z0-9]")
 mod_id_regex: re.Pattern = re.compile(r"[A-z0-9\.\-_]+")
@@ -11,12 +13,17 @@ def is_exe(exe: str):
 
 
 def validate_mod_id(id: str) -> bool:
+    if id is None:
+        return False
     return (
-        mod_id_regex.fullmatch(id)
-        and strict_mod_id_regex.fullmatch(id[0])
-        and strict_mod_id_regex.fullmatch(id[-1])
+        mod_id_regex.fullmatch(id) is not None
+        and strict_mod_id_regex.fullmatch(id[0]) is not None
+        and strict_mod_id_regex.fullmatch(id[-1]) is not None
     )
 
 
 def suggest_mod_id(id: str) -> str:
     return inverse_mod_id_regex.subn("-", id)[0].strip(".-_")
+
+def get_username() -> str:
+    return pwd.getpwuid(os.getuid()).pw_name
